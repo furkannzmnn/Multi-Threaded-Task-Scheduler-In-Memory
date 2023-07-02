6b49568cc35cebbde1fa42910f55b466a3a7f1ed
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2023-07-02 14:23:01 +0300

    Update Denem.java

--- src/main/java/org/example/Denem.java
+++ src/main/java/org/example/Denem.java
@@ -1,12 +1,1 @@
-package org.example;
 
-public class Denem {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
-
-
-class OneTimeTask2  extends Main {
-    
-}
